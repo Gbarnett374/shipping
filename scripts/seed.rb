e55233ccd7adaf21d42b129a1da7a7cbba134825
Gbarnett374 <Gbarnett374@gmail.com>
2018-04-22 16:05:00 -0400

    a few small tweeks.

--- scripts/seed.rb
+++ scripts/seed.rb
@@ -1,9 +1,5 @@
 require 'json'
 products_file = File.read('products.json')
-# Read file into hash 
 products_hash = JSON.parse(products_file).first[1]
-
-products_hash.each do |h|
-  @product = Product.new(h).save
-end
+products_hash.each { |h| @product = Product.new(h).save }
 puts "#{Product.count} records created!"
