bfbdc9955d2496f59fa03542295b5a76a05df41e
Gbarnett374 <Gbarnett374@gmail.com>
2018-04-23 14:21:34 -0400

    Modal for calculating works.

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -1,6 +1,26 @@
 $(document).ready(() => {
-    console.log('hi');
     $('#calculator-btn').click(() => {
         $('#calculator-modal').modal('toggle');
     }); 
+
+    $('#calculator-form').submit(e => {
+        e.preventDefault();
+        let formData = {};
+        $('#calculator-form').serializeArray().forEach(field => {
+            formData[field.name] = field.value
+        });
+        let url = `/products/${formData.length}/${formData.width}`
+        url+= `/${formData.height}`;
+        url+= `/${formData.weight}.json `;
+
+        $.ajax({
+            url: url,
+            type: 'GET'
+        }).done(data => {
+            $('#search-results').text(`Your Result: ${data.name}`);
+            console.log(data);
+        }).fail((jqXHR, textStatus, errorThrown) => {
+            $('#search-results').text('Sorry there was no results.');
+        });
+    });
 });
