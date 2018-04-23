272ab5763be040767e19d06af313f2f9058d7abc
Gbarnett374 <Gbarnett374@gmail.com>
2018-04-23 14:38:07 -0400

    more styling changes.

--- app/assets/javascripts/calculator.js
+++ app/assets/javascripts/calculator.js
@@ -17,14 +17,17 @@ $(document).ready(() => {
             url: url,
             type: 'GET'
         }).done(data => {
+            $('#search-results').addClass('text-success');
             $('#search-results').text(`Your Result: ${data.name}`);
             setTimeout(() => {
+                $('#jumbotron-search-results').addClass('text-success');
                 $('#jumbotron-search-results').text(`Your Result: ${data.name}`);
                 $('#calculator-modal').modal('toggle');
                 $('#calculator-form')[0].reset();
             }, 5000);
             console.log(data);
         }).fail((jqXHR, textStatus, errorThrown) => {
+            $('#search-results').addClass('text-danger');
             $('#search-results').text('Sorry there was no results.');
         });
     });
